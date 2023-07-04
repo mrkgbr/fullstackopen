cf30f5f6e6f6a9dda6c46bde205ef91260fa64b9
mrkgbr <116895777+mrkgbr@users.noreply.github.com>
2023-07-04 10:23:13 +0200

    add Part component, refactor Content

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -11,18 +11,20 @@ const App = () => {
     return <h1>{props.course}</h1>;
   };
 
+  const Part = (props) => {
+    return (
+      <p>
+        {props.part} {props.exercises}
+      </p>
+    );
+  };
+
   const Content = (props) => {
     return (
       <>
-        <p>
-          {props.part1} {props.exercises1}
-        </p>
-        <p>
-          {props.part2} {props.exercises2}
-        </p>
-        <p>
-          {props.part3} {props.exercises3}
-        </p>
+        <Part part={part1} exercises={exercises1} />
+        <Part part={part2} exercises={exercises2} />
+        <Part part={part3} exercises={exercises3} />
       </>
     );
   };
