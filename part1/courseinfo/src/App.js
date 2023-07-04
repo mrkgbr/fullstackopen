9963d70a835533ad69bc7d8709eb6445fd3a0917
mrkgbr <116895777+mrkgbr@users.noreply.github.com>
2023-07-04 10:20:00 +0200

    refactor, add new components

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -7,19 +7,51 @@ const App = () => {
   const part3 = "State of a component";
   const exercises3 = 14;
 
+  const Header = (props) => {
+    return <h1>{props.course}</h1>;
+  };
+
+  const Content = (props) => {
+    return (
+      <>
+        <p>
+          {props.part1} {props.exercises1}
+        </p>
+        <p>
+          {props.part2} {props.exercises2}
+        </p>
+        <p>
+          {props.part3} {props.exercises3}
+        </p>
+      </>
+    );
+  };
+
+  const Total = (props) => {
+    return (
+      <p>
+        Number of exercises{" "}
+        {props.exercises1 + props.exercises2 + props.exercises3}
+      </p>
+    );
+  };
+
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Header course={course} />
+      <Content
+        part1={part1}
+        part2={part2}
+        part3={part3}
+        exercises1={exercises1}
+        exercises2={exercises2}
+        exercises3={exercises3}
+      />
+      <Total
+        exercises1={exercises1}
+        exercises2={exercises2}
+        exercises3={exercises3}
+      />
     </div>
   );
 };
