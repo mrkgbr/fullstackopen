7635674d85837745b24a9c60d91c2d1ab1c0a0a2
mrkgbr <116895777+mrkgbr@users.noreply.github.com>
2023-07-10 14:16:48 +0200

    Updated to 2.11

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,20 +1,26 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Form from "./components/Form";
 import Numbers from "./components/Numbers";
 import Title from "./components/Title";
 import { Filter } from "./components/Filter";
+import axios from "axios";
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456", id: 1 },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [filter, setFilter] = useState("");
-  const [filteredList, setFilteredList] = useState(persons);
+  const [filteredList, setFilteredList] = useState([]);
+
+  useEffect(() => {
+    console.log("effect");
+    axios.get("http://localhost:3001/persons").then((response) => {
+      console.log("promise fulfilled");
+      const data = response.data;
+      setPersons(data);
+      setFilteredList(data);
+    });
+  }, []);
 
   const handleNameChange = (event) => {
     setNewName(event.target.value);
