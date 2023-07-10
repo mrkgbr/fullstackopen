271165bd7dd58f67ae15737020bc5b9bfd0f5562
mrkgbr <116895777+mrkgbr@users.noreply.github.com>
2023-07-10 17:45:41 +0200

    Updated to 2.15

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -43,15 +43,21 @@ const App = () => {
     setFilteredList(newFilteredList);
   };
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    const checkName = persons.find(
-      (person) => person.name.toLowerCase() === newName.toLowerCase()
-    );
-    if (checkName) {
-      alert(`${newName} is already added to phonebook`);
-      return;
-    }
+  const handleUpdate = (checkName) => {
+    const number = persons.find((number) => number.id === checkName.id);
+    const changedNumber = { ...number, number: newNumber };
+    numberService.update(checkName.id, changedNumber).then((returnedNumber) => {
+      const newPersons = persons.map((number) =>
+        number.id !== checkName.id ? number : returnedNumber
+      );
+      setPersons(newPersons);
+      setFilteredList(newPersons);
+      setNewName("");
+      setNewNumber("");
+    });
+  };
+
+  const handleAdd = () => {
     const newObject = {
       name: newName,
       number: newNumber,
@@ -64,6 +70,28 @@ const App = () => {
       setNewName("");
       setNewNumber("");
     });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const checkName = persons.find(
+      (person) => person.name.toLowerCase() === newName.toLowerCase()
+    );
+
+    if (checkName) {
+      if (
+        window.confirm(
+          `${checkName.name} is already added to phonebook, replace old number with a new?`
+        )
+      ) {
+        handleUpdate(checkName);
+        return;
+      } else {
+        return;
+      }
+    }
+
+    handleAdd();
   };
 
   const handleRemove = (name, id) => {
